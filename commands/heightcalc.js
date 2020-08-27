78fe48802f8f8935be77e02af3a16dc8d989565d
YugenOfficial <65828710+YugenOfficial@users.noreply.github.com>
2020-08-27 20:09:40 +0300

    dfdf

--- commands/heightcalc.js
+++ commands/heightcalc.js
@@ -7,9 +7,9 @@ module.exports.run = async (bot, message, args) =>{
         message.reply('in order for me to calculate your height, you will need to tell me your height in centimeters. Correct usage: `scheightcalc <height>`')
     } else if (isNaN(args[1])){
         message.reply('your height needs to be a number.')
-    } else if (args[1]>271 || args[1]<50){
+    } else if ((args[1]>271) || (args[1]<50)){
         message.reply('stop bullshitting me.')
-    } else if (args[1] == 272){
+    } else if (args[1] === 272){
         message.reply('you are as tall as the tallest man who ever lived! Or you are just lying, which is probably the case.')
     } else message.reply(`your height is ${args[1]}cm!`)
     
