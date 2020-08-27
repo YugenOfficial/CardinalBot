d23e33817adbecfc988b44a649a286d30afd79d8
YugenOfficial <65828710+YugenOfficial@users.noreply.github.com>
2020-08-27 19:59:19 +0300

    fg

--- commands/heightcalc.js
+++ commands/heightcalc.js
@@ -4,22 +4,14 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async (bot, message, args) =>{
 
     if(!args[1]){
-        return message.reply('in order for me to calculate your height, you will need to tell me your height in centimeters. Correct usage: `scheightcalc <height>`')
-    }
-
-    if (isNaN(args[1])){
-        return message.reply('your height needs to be a number.')
-    }
-
-    if(args[1]>271 || args[1]<50){
-        return message.reply('stop bullshitting me.')
-    }
-
-    if(args[1] == 272){
-        return message.reply('you are as tall as the tallest man who ever lived! Or you are just lying, which is probably the case.')
-    }
-
-    message.reply(`your height is ${args[0]}cm!`)
+        message.reply('in order for me to calculate your height, you will need to tell me your height in centimeters. Correct usage: `scheightcalc <height>`')
+    } else if (isNaN(args[1])){
+        message.reply('your height needs to be a number.')
+    } else if (args[1]>271 || args[1]<50){
+        message.reply('stop bullshitting me.')
+    } else if (args[1] == 272){
+        message.reply('you are as tall as the tallest man who ever lived! Or you are just lying, which is probably the case.')
+    } else message.reply(`your height is ${args[0]}cm!`)
     
 }
         
