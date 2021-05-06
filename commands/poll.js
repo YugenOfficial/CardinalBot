26b4f8f4dbbc8d9a2be1740c33c4c124976ae6fe
YugenOfficial <65828710+YugenOfficial@users.noreply.github.com>
2021-05-06 15:04:53 +0300

    dsfdf

--- commands/poll.js
+++ commands/poll.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) =>{
 
     const arg = message.content.trim().split(/ +/g);
 
-// Defining the question...
-let question = [];
+
+    let question = [];
 
 for (let i = 1; i < args.length; i++) {
   if (arg[i].startsWith('"')) break;
@@ -15,19 +15,18 @@ for (let i = 1; i < args.length; i++) {
 
 question = question.join(' ');
 
-// Defining the choices...
+
 const choices = [];
 
 const regex = /(["'])((?:\\\1|\1\1|(?!\1).)*)\1/g;
 let match;
 while (match = regex.exec(arg.join(' '))) choices.push(match[2]);
 
-// Creating and sending embed...
 let content = [];
 for (let i = 0; i < choices.length; i++) content.push(`${options[i]} ${choices[i]}`);
 content = content.join('\n');
 
-var embed = new Discord.RichEmbed()
+var embed = new Discord.MessageEmbed()
   .setColor('#8CD7FF')
   .setTitle(`**${question}**`)
   .setDescription(content);
